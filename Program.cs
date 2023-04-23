e1990eede15ae6c00516216f5606ce2091b4bd69
Aryan Prajapati <95575009+Aryanp1757@users.noreply.github.com>
2023-04-23 18:43:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Minutesandseconds
+{
+    internal class Program
+    {   /// <summary>
+    /// demostartes integer data types
+    /// </summary>
+    /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            int totalsecondsplayed = 100;
+            const int secondspermiute = 60;
+            // calculated minutes and seconds 
+
+            int minutesplayed= totalsecondsplayed/secondspermiute;
+            int secondsplayed = totalsecondsplayed % secondspermiute;
+
+            // print minutes and seconds 
+            Console.WriteLine("total minutes : " + minutesplayed);
+            Console.WriteLine("seconds played : " + secondsplayed); 
+        }
+    }
+}
